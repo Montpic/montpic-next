0c67012a14220b3aaffe0b1599ef6b40547c22e4
TintinLeGrand <ethan.leneindre9@gmail.com>
2025-04-14 23:26:43 +0200

    fix: Remove unnecessary import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 const spaceMono = Space_Mono({
   variable: "--font-space-mono",
