bef0e5f5fd40c7d81e5d904ca6ee9570499e95c3
TintinLeGrand <ethan.leneindre9@gmail.com>
2025-04-14 22:38:28 +0200

    fix: Redirection with key

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -3,29 +3,32 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
-  for (const activity of Object.values(activities)) {
-    const { name, location, activity: act, image, members } = activity;
-    return (
-      <div className="flex flex-col items-center justify-center text-center gap-4 h-screen w-screen">
-        <Link
-          href={`/activities/${activity.name}`}
-          className="flex items-center justify-center text-center p-2 gap-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out"
-        >
-          <Image
-            src={activity.image}
-            alt={activity.name}
-            width={64}
-            height={64}
-            className="rounded-full aspect-square"
-          />
-          <div className="flex flex-col items-center justify-center text-center gap-4">
-            <p>
-              {name} - {location}
-            </p>
-            <p>{act}</p>
-          </div>
-        </Link>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col items-center justify-center text-center gap-4 h-screen w-screen">
+      {Object.entries(activities).map(([key, activity]) => {
+        const { name, location, activity: act, image } = activity;
+        return (
+          <Link
+            key={key}
+            href={`/activities/${key}`}
+            className="flex items-center justify-center text-center p-2 gap-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out"
+          >
+            <Image
+              src={image}
+              alt={name}
+              width={64}
+              height={64}
+              className="rounded-full aspect-square"
+            />
+            <div className="flex flex-col items-center justify-center text-center gap-4">
+              <p>
+                {name} - {location}
+              </p>
+              <p>{act}</p>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
