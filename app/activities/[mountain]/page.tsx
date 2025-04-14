e9390c66661d91ac2dd638eceb7c399eca5a8030
TintinLeGrand <ethan.leneindre9@gmail.com>
2025-04-14 22:22:47 +0200

    feat: Add data and display page

---
+++ app/activities/[mountain]/page.tsx
@@ -0,0 +1,42 @@
+import activities from "@/lib/activities.json";
+import Image from "next/image";
+
+type Activities = {
+  [key: string]: {
+    name: string;
+    location: string;
+    activity: string;
+    image: string;
+    members: { pseudo: string; age: number }[];
+  };
+};
+
+const typedActivities: Activities = activities;
+import { notFound } from "next/navigation";
+
+export default async function Page({
+  params,
+}: {
+  params: { mountain: string };
+}) {
+  const { mountain } = params;
+  if (!(mountain in typedActivities)) {
+    notFound();
+  }
+
+  const activity = typedActivities[mountain];
+  return (
+    <div className="flex flex-col items-center justify-center text-center min-h-screen p-2 pt-10 gap-4">
+      <h1 className="text-3xl">{activity.name}</h1>
+      <p>{activity.location}</p>
+      <Image
+        src={activity.image}
+        alt={activity.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+      />
+      <p>{activity.activity}</p>
+    </div>
+  );
+}
