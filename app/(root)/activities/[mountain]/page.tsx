d01c42193adc539175ba97f199cc67da30b267b5
TintinLeGrand <ethan.leneindre9@gmail.com>
2025-04-30 00:40:58 +0200

    fix: Type of mountain

--- app/(root)/activities/[mountain]/page.tsx
+++ app/(root)/activities/[mountain]/page.tsx
@@ -1,55 +1,59 @@
 import activities from "@/lib/activities.json";
 import Image from "next/image";
 import { Activities } from "@/types/Activity";
-
-const typedActivities: Activities = activities;
 import { notFound } from "next/navigation";
 import { Button, Popover } from "flowbite-react";
 
-export default async function Page({
+const typedActivities: Activities = activities;
+
+export default function Page({
   params,
 }: {
   params: { mountain: string };
 }) {
   const { mountain } = params;
+
   if (!(mountain in typedActivities)) {
     notFound();
   }
 
   const activity = typedActivities[mountain];
   return (
-    <div className="flex md:flex-row flex-col grow items-center justify-center text-center p-2 pt-10 gap-4">
-      <div className="flex flex-col items-center justify-center text-center gap-4 rounded-lg shadow-md border p-4">
-        <h1 className="text-3xl">{activity.name}</h1>
-        <p>{activity.location}</p>
-        <Image
-          src={activity.image}
-          alt={activity.name}
-          width={500}
-          height={500}
-          className="rounded-lg"
-        />
-      </div>
+    <div className="flex flex-col items-center justify-center text-center">
+      <div className="flex md:flex-row flex-col grow items-center justify-center text-center p-2 pt-10 gap-4">
+        <div className="flex flex-col items-center justify-center text-center gap-4 rounded-lg shadow-md border p-4">
+          <h1 className="text-3xl">{activity.name}</h1>
+          <p>{activity.location}</p>
+          <Image
+            src={activity.image}
+            alt={activity.name}
+            width={500}
+            height={500}
+            className="rounded-lg"
+          />
+        </div>
 
-      <div className="flex flex-col items-center justify-center text-center gap-4">
-        <p>
-          <u>Activité :</u> {activity.activity}
-        </p>
-        <p>
-          Inscrits à la prochaine activité le{" "}
-          {new Date(Date.now() + 86400000).toLocaleDateString("fr-FR")}:
-        </p>
-        {activity.members.map((member) => (
-          <div
-            key={member.pseudo}
-            className="flex flex-col items-start lg:items-end lg:text-right"
-          >
-            <OverProfile memberName={member.pseudo}>
-              {member.pseudo}
-            </OverProfile>
-          </div>
-        ))}
+        <div className="flex flex-col items-center justify-center text-center gap-4">
+          <p>
+            <u>Activité :</u> {activity.activity}
+          </p>
+          <p>
+            Inscrits à la prochaine activité le{" "}
+            {new Date(Date.now() + 86400000).toLocaleDateString("fr-FR")}:
+          </p>
+          {activity.members.map((member) => (
+            <div
+              key={member.pseudo}
+              className="flex flex-col items-start lg:items-end lg:text-right"
+            >
+              <OverProfile memberName={member.pseudo}>
+                {member.pseudo}
+              </OverProfile>
+            </div>
+          ))}
+        </div>
       </div>
+      <Button color="light">S'inscrire</Button>
     </div>
   );
 }
